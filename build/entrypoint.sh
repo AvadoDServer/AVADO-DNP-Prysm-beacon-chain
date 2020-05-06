ceed550bc9aa1bbabc17c5bffe4be86f834fbcfd
stefaan <stefaan@ponnet.com>
2020-05-07 00:13:53 +0200

    new release

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -5,7 +5,7 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-COMMAND="app/beacon-chain/image.binary.runfiles/prysm/beacon-chain/linux_amd64_stripped/image.binary --datadir=/data --log-file=/data/beacon-chain.log ${EXTRA_OPTS}"
+COMMAND="app/beacon-chain/image.binary.runfiles/prysm/beacon-chain/linux_amd64_stripped/image.binary --datadir=/data --grpc-gateway-port 4001 --log-file=/data/beacon-chain.log ${EXTRA_OPTS}"
 #COMMAND="app/beacon-chain/image.binary.runfiles/prysm/beacon-chain/linux_amd64_stripped/image.binary -h"
 
 supervisord -c /etc/supervisord.conf &
