967fc6e7e8950fde2613428986c2f2042a375aa1
stefaan <stefaan@ponnet.com>
2020-10-28 18:10:28 +0100

    bump to v1.0.0-beta.0.rc

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -5,7 +5,7 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-COMMAND="app/beacon-chain/image.binary.runfiles/prysm/beacon-chain/linux_amd64_stripped/image.binary --rpc-host=0.0.0.0 --rpc-port=4000 --datadir=/data --enable-byte-mempool --enable-state-ref-copy --grpc-gateway-port 4001 --log-file=/data/beacon-chain.log --block-batch-limit=500 ${EXTRA_OPTS}"
+COMMAND="app/beacon-chain/image.binary.runfiles/prysm/beacon-chain/linux_amd64_stripped/image.binary --rpc-host=0.0.0.0 --rpc-port=4000 --datadir=/data --accept-terms-of-use --grpc-gateway-port 4001 --log-file=/data/beacon-chain.log ${EXTRA_OPTS}"
 
 supervisord -c /etc/supervisord.conf &
 
