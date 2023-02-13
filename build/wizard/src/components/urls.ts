b487edd7bf8b8f076f0466feb58659656394a61c
Hendrik Eeckhaut <hendrik@eeckhaut.org>
2023-02-13 20:59:50 +0100

    Prysm v3.2.1

--- build/wizard/src/components/urls.ts
+++ build/wizard/src/components/urls.ts
@@ -1,5 +1,5 @@
-export const packageName = "prysm-beacon-chain-mainnet.avado.dnp.dappnode.eth";
-export const validator_packageName = "eth2validator.avado.dnp.dappnode.eth";
-export const packageUrl = "prysm-beacon-chain-mainnet.my.ava.do";
-export const keyManagerAPIUrl = "http://eth2validator.my.ava.do:7500"
+export const packageName = "prysm-beacon-chain-prater.avado.dnp.dappnode.eth";
+export const validator_packageName = "eth2validator-prater.avado.dnp.dappnode.eth";
+export const packageUrl = "prysm-beacon-chain-prater.my.ava.do";
+export const keyManagerAPIUrl = "http://eth2validator-prater.my.ava.do:7500"
 
